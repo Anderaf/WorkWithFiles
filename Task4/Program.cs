100eaf74b949cd392c6ab2e8cf0cd6417b681e6d
Anderaf <kuldperea@gmail.com>
2022-12-11 15:54:59 +1000

    Проверки для последнего задания

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -8,40 +8,50 @@ namespace FinalTask
     {
         static void Main(string[] args)
         {
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            string directoryPath = desktopPath + @"\students";
-            var directory = new DirectoryInfo(directoryPath);
-
-            if (!directory.Exists)
-                directory.Create();
-
-            string binaryFilePath = desktopPath + @"\Students.dat";
-            var binaryFile = new FileInfo(binaryFilePath);
-
-            Console.WriteLine(binaryFilePath);
-            if (binaryFile.Exists)
+            try
             {
-                Console.WriteLine(1);
-                BinaryFormatter formatter = new BinaryFormatter();
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                string directoryPath = desktopPath + @"\students";
+                var directory = new DirectoryInfo(directoryPath);
 
-                Student[] students;
+                if (!directory.Exists)
+                    directory.Create();
 
-                using (var fs = new FileStream(binaryFilePath,FileMode.OpenOrCreate))
-                {                   
-                    students = (Student[])formatter.Deserialize(fs);
-                }
+                string binaryFilePath = desktopPath + @"\Students.dat";
+                var binaryFile = new FileInfo(binaryFilePath);
 
-                for (int i = 0; i < students.Length; i++)
-                {                    
-                    string filePath = directoryPath + @"\" + students[i].Group + ".txt";
-                    FileInfo file = new FileInfo(filePath);
+                if (binaryFile.Exists)
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
 
-                    using (StreamWriter sw = file.AppendText())
+                    Student[] students;
+
+                    using (var fs = new FileStream(binaryFilePath, FileMode.OpenOrCreate))
                     {
-                        sw.WriteLine("{0}, {1}", students[i].Name, students[i].DateOfBirth);
+                        students = (Student[])formatter.Deserialize(fs);
+                    }
+
+                    for (int i = 0; i < students.Length; i++)
+                    {
+                        string filePath = directoryPath + @"\" + students[i].Group + ".txt";
+                        FileInfo file = new FileInfo(filePath);
+
+                        using (StreamWriter sw = file.AppendText())
+                        {
+                            sw.WriteLine("{0}, {1}", students[i].Name, students[i].DateOfBirth);
+                        }
                     }
                 }
+                else
+                {
+                    Console.WriteLine("Бинарный файл не найден");
+                }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: {0}", e);
+            }
+            
         }
     }
     [Serializable]
